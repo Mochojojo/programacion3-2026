e0efb6a2358daa6c77a99bb2f2746e5de3d6988b
Mochojojo <mochojojo64@gmail.com>
2026-02-11 10:00:17 -0700

    formulario

--- src/practica1/MiPanel.java
+++ src/practica1/MiPanel.java
@@ -1,6 +1,7 @@
 package practica1;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -8,14 +9,18 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
 
 public class MiPanel extends JPanel{
 	
 	public MiPanel() {
-		Font fuente = new Font("Impact", Font.BOLD, 20);
-		setBackground(new Color(177, 26, 242)); //rgb
+		Font fuente = new Font("Georgia", Font.BOLD, 20);
+		setBackground(Color.GRAY); //rgb
 		setLayout(null);
 		
+		formulario(fuente);
 		boton();
 		
 		JLabel saludo = new JLabel("Bienvenido!");
@@ -23,13 +28,61 @@ public class MiPanel extends JPanel{
 		
 	}
 	
+	private void formulario(Font fuente) {
+		JLabel lblSaludo = new JLabel("Bienvenido!");
+		lblSaludo.setForeground(Color.BLACK);
+		lblSaludo.setFont(fuente);
+		lblSaludo.setBounds(190,0,200,40);
+		add(lblSaludo);
+		
+		JLabel lblEmail = new JLabel("Email: ");
+		lblEmail.setForeground(Color.BLACK);
+		lblEmail.setFont(fuente);
+		lblEmail.setBounds(10,100,200,40);
+		add(lblEmail);
+		
+		JTextField texto = new JTextField();
+		texto.setForeground(Color.BLUE);
+		texto.setFont(fuente);
+		texto.setBounds(150,100,200,40);
+		add(texto);
+		
+		JLabel lblAlertaEmail = new JLabel("Ingresa un email");
+		lblAlertaEmail.setForeground(Color.BLACK);
+		lblAlertaEmail.setFont(fuente);
+		lblAlertaEmail.setBounds(150,130,200,40);
+		add(lblAlertaEmail);
+		
+		JLabel lblContrasena = new JLabel("Contraseña: ");
+		lblContrasena.setForeground(Color.BLACK);
+		lblContrasena.setFont(fuente);
+		lblContrasena.setBounds(10,200,200,40);
+		add(lblContrasena);
+		
+		JPasswordField contrasena = new JPasswordField();
+		contrasena.setForeground(Color.BLUE);
+		contrasena.setFont(fuente);
+		contrasena.setBounds(150,200,200,40);
+		add(contrasena);
+		
+		JLabel lblAlertaPassword = new JLabel("Contrasena incorrecta");
+		lblAlertaPassword.setForeground(Color.BLACK);
+		lblAlertaPassword.setFont(fuente);
+		lblAlertaPassword.setBounds(150,200,1000,100);
+		add(lblAlertaPassword);
+	}
+	
 	public void boton() {
-		JButton boton = new JButton("Mi boton");
-		boton.setBounds(50,50,200,30); //x,y,ancho,altura
+		JButton boton = new JButton("Iniciar sesion");
+		boton.setBounds(150,300,200,30); //x,y,ancho,altura
 		boton.setBackground(Color.GREEN);
-		boton.setForeground(Color.RED);
+		boton.setForeground(Color.BLACK);
 		boton.setToolTipText("fasewvniefcn");
 		boton.setFont(new Font("Arial", Font.ITALIC, 20));
+		boton.setBorder(BorderFactory.createLineBorder(Color.ORANGE, 3, true));
+		boton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.GREEN, Color.YELLOW, Color.PINK, Color.ORANGE));
+		boton.setBorder(BorderFactory.createDashedBorder(Color.GREEN, 3, 5, 1, true));
+		boton.setToolTipText("Soy un tooltip");
 		
 		imagen(boton);
 		
@@ -38,12 +91,18 @@ public class MiPanel extends JPanel{
 	
 	public void imagen(JButton boton){
 		try {
-			Image icono = ImageIO.read(getClass().getResource("../img/roblo.png"));
+			Image icono = ImageIO.read(getClass().getResource("../img/iconoLogin.png"));
 			icono = icono.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
 			boton.setIcon(new ImageIcon(icono));
 		}
 		catch(Exception ex){
 			System.out.println("No se encontro la imagen");
 		}
+	}
+	
+	private void crearLogo() {
+		JLabel lblSaludo = new JLabel();
+		lblSaludo.setBounds(125, 50, 50, 50);
+		//lblSaludo.Icon(new ImageIcon("src/img/roblo.png"));
 	}
 }
--- src/practica1/Ventana.java
+++ src/practica1/Ventana.java
@@ -10,14 +10,14 @@ public class Ventana extends JFrame{
 		setSize(500, 500); //tamano de la ventana
 		setLocation(0,0); //coordenadas de inicio en la pantalla
 		setBounds(100, 100, 500, 500);//tamano del rectangulo
-		setTitle("nigga"); //titulo de la ventana
+		setTitle("Tienda"); //titulo de la ventana
 		setLocationRelativeTo(null); //posicion relativa a un componente, si es null aparece en el centro
 		setResizable(false); //ajustar tamano de la ventana con el cursor
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //dejar de ejecutar el programa al cerrar la ventana
 		
 		//icono de la ventana
 		Toolkit tk = Toolkit.getDefaultToolkit();
-		Image icono = tk.getImage("src/practica1/img/icono.jpg"); //necesario crear una carpeta img
+		Image icono = tk.getImage("src/img/iconoTienda.jpg"); //necesario crear una carpeta img
 		setIconImage(icono);
 		
 		MiPanel panelito = new MiPanel();
